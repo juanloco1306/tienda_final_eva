1ff38265518613f6cbe73ba72b3b995b00f72d9b
juanloco1306 <juanloco1306@gmail.com>
2022-09-11 01:09:07 -0500

    integrando pixel creooo

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
-
+import Script from 'next/script';
 import { Layout } from '../components';
 
 import '../styles/globals.css'
@@ -14,6 +14,21 @@ function MyApp({ Component, pageProps }) {
     <StateContext>
     <Layout>
       <Toaster/>
+      <Script src='https://www.facebook.com/tr?id=516952770431137&ev=PageView&noscript=1'/>
+      <Script id='facebook-script'>
+        { `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '516952770431137');
+        fbq('track', 'PageView');
+        `}
+      </Script>
        <Component {...pageProps} />
        </Layout>
        </StateContext>
