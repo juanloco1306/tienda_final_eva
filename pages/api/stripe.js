628194bdbbd604b2c88e67d69f8cec17589102b2
juanloco1306 <juanloco1306@gmail.com>
2022-09-12 08:38:16 -0500

    lo arregle

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -8,9 +8,6 @@ export default async function handler(req, res) {
       const params = {
         submit_type: 'pay',
         mode: 'payment',
-        discounts: [{
-          coupon: 'promo_1LhCPHDo11d76qd2jjNutkCA',
-        }],
         payment_method_types: ['card'],
         billing_address_collection: 'required',
         phone_number_collection: {
