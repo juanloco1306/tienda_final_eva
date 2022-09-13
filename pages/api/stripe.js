f233c0dd917ce82124cdc310831b3814d7211ac5
juanloco1306 <juanloco1306@gmail.com>
2022-09-13 00:02:21 -0500

    terminando tienda

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -12,7 +12,6 @@ export default async function handler(req, res) {
           coupon: 'MAMASITA',
           code: '',
         }],*/
-        allow_promotion_codes: true,
         payment_method_types: ['card'],
         billing_address_collection: 'required',
         phone_number_collection: {
