d90ba4da8b89db8e5e6ad79d62f3eed382910995
juanloco1306 <juanloco1306@gmail.com>
2022-10-11 16:26:06 -0500

    ultima version 11 de octubre

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <p className="footer__copy">&#169; 2022 El JUJI. Todos los derechos reservados</p>
+    <p className="footer__copy">&#169; 2022 ELJUJI. Todos los derechos reservados</p>
   )
 }
 
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -62,16 +62,7 @@ const ProductDetails = ({ product, products }) => {
             </div>
         </div>
 
-        <div className="maylike-products-wrapper">
-          <h2>Otros Productos</h2>
-          <div className="marquee">
-            <div className="maylike-products-container track">
-              {products.map((item) => (
-                <Product key={item._id} product={item} />
-              ))}
-            </div>
-          </div>
-      </div>
+
 
 
 
